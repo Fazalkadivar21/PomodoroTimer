582f738b4808ef77e3898be471ab985d5c520fd7
fazalkadivar21 <fazalkadivar21@gmail.com>
2024-11-14 10:18:52 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Timer from "./components/Timer";
+import Settings from "./components/Settings";
+
+function App() {
+  return (
+    <>
+      <Timer />
+      <Settings />
+    </>
+  );
+}
+
+export default App;
